4163bb52f808a95dbdd20ea7ffeb0b1224ec2225
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-12-12 19:51:06 -0500

    Fix Model#where to handle responses of a single item in a collection

--- spec/angular-resource/mocks/system.js
+++ spec/angular-resource/mocks/system.js
@@ -1,18 +1,21 @@
 angular
   .module('ActiveResource.Mocks')
   .provider('ARMockSystem', function() {
+
     this.$get = ['ActiveResource', 'ARMockSensor',
       function(ActiveResource, Sensor) {
+
         function System(data) {
           if (!data) data = {};
           this.id        = data.id        || undefined;
           this.placement = data.placement || undefined;
+          this.name      = data.name      || undefined;
           this.hasMany('sensors',
             ['ActiveResource.Mocks', 'ARMockSensor']);
         };
 
         System = ActiveResource.Base.apply(System);
-        System.api.set('http://api.faculty.com/');
+        System.api.set('http://api.faculty.com');
 
         return System;
     }];
