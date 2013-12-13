8759e0f77450f6dccbc94acaaf107593f6dbc922
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-12-13 15:22:22 -0500

    Add mock settable properties to post mock

--- spec/angular-resource/mocks/post.js
+++ spec/angular-resource/mocks/post.js
@@ -11,7 +11,22 @@ angular
         
         this.belongsTo('author');
         this.hasMany('comments');
+
+        var content;
+        Object.defineProperty(this, 'content', {
+          enumerable: true,
+          get: function()    { return content; },
+          set: function(val) { content = val;  }
+        });
+
       };
+
+      var date;
+      Object.defineProperty(Post.prototype, 'date', {
+        enumerable: true,
+        get: function()    { return date; },
+        set: function(val) { date = val;  }
+      });
 
       Post = ActiveResource.Base.apply(Post);
       Post.dependentDestroy('comments');
