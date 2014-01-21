dd8613e23957352db4b6a4b70fe5f95cdcaf855a
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-01-21 12:55:42 -0500

    Add computed property mock

--- spec/angular-resource/mocks/tshirt.js
+++ spec/angular-resource/mocks/tshirt.js
@@ -8,9 +8,35 @@ angular
         this.number('price');
         this.boolean('available');
         this.string('name');
+        this.computedProperty('salePrice', function() {
+          return this.price - (this.price * 0.2);
+        }, 'price');
+
+        this.computedProperty('superSalePrice', function() {
+          return this.price - this.salePrice;
+        }, ['price', 'salePrice']);
+
+        this.computedProperty('superDuperSalePrice', function() {
+          return this.superSalePrice - this.salePrice - this.price + ' Wow! We owe YOU money!';
+        }, ['price', 'salePrice', 'superSalePrice']);
+
+        this.computedProperty('allTheProperties', function() {
+          return [this.name, this.available, this.price, this.order_id];
+        }, ['order_id', 'price', 'available', 'name']);
+
+        this.computedProperty('prices', function() {
+          var instance = this;
+          return {
+            price: instance.price,
+            salePrice: instance.salePrice,
+            superSalePrice: instance.superSalePrice,
+            superDuperSalePrice: instance.superDuperSalePrice
+          };
+        }, ['price', 'salePrice', 'superSalePrice', 'superDuperSalePrice']);
 
         this.validates({
-          size: { inclusion: { in: ['small', 'medium', 'large'] } }
+          size:      { inclusion: { in: ['small', 'medium', 'large'] } },
+          salePrice: { numericality: true }
         });
       }
 
