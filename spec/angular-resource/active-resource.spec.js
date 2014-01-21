ed45c02a2991f9ba99b139fe843bf175db20ff82
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-01-21 12:55:31 -0500

    Add computed property spec

--- spec/angular-resource/active-resource.spec.js
+++ spec/angular-resource/active-resource.spec.js
@@ -209,6 +209,37 @@ describe('ActiveResource', function() {
         expect(tshirt.$valid).toBe(false);
       });
     });
+
+    describe('Computed Properties', function() {
+      it('computes properties based on other properties', function() {
+        expect(tshirt.salePrice).toEqual(8.792);
+      });
+
+      it('computes the property during any change', function() {
+        tshirt.price = '20.00';
+        expect(tshirt.salePrice).toEqual(16);
+      });
+      
+      it('creates complex chains of computed properties', function() {
+        tshirt.price = '20.00';
+        expect(tshirt.superSalePrice).toEqual(4);
+      });
+
+      it('creates complex chains of computed properties', function() {
+        tshirt.price = '20.00';
+        expect(tshirt.superDuperSalePrice).toEqual('-32 Wow! We owe YOU money!');
+      });
+
+      it('creates computed arrays', function() {
+        tshirt.price = '20.00';
+        expect(tshirt.allTheProperties).toEqual([ 'Ragin Shirt', true, '20.00', 1 ]);
+      });
+
+      it('creates computed objects', function() {
+        tshirt.price = '20.00';
+        expect(tshirt.prices).toEqual({ price : '20.00', salePrice : 16, superSalePrice : 4, superDuperSalePrice : '-32 Wow! We owe YOU money!' });
+      });
+    });
   });
 
   describe('Caching', function() {
